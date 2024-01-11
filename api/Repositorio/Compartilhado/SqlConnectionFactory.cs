682b7e88c0045d969ddf0fcfc567446570a7df1d
Mateus Lázaro <22450297+matl4zaro@users.noreply.github.com>
2024-01-11 08:16:49 -0300

    API | Configuração de conexão com banco e adição de métodos genéricos encapsulando dapper contrib e factory para criação da conexão do banco
    (cherry picked from commit 0a1ba8d82b21922d94df61c719dcda86fba086ee)

---
+++ api/Repositorio/Compartilhado/SqlConnectionFactory.cs
@@ -0,0 +1,25 @@
+﻿using Interfaces.Repositorio;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorio.Compartilhado;
+
+public class SqlConnectionFactory : ISqlConnectionFactory
+{
+    private readonly IConfiguration _configuration;
+
+    public SqlConnectionFactory(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public SqlConnection CreateConnection()
+    {
+        return new SqlConnection(_configuration.GetConnectionString("ProjetoMarvelDB"));
+    }
+}
---
+++ api/Repositorio/Contexto/DapperContexto.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Extensions.Configuration;
+using Repositorio.Compartilhado;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper.Contrib;
+using Dapper.Contrib.Extensions;
+
+namespace Repositorio.Contexto;
+
+public class DapperContexto : SqlConnectionFactory
+{
+    public DapperContexto(IConfiguration configuration) : base(configuration)
+    {
+    }
+
+    public T Cadastrar<T>(T entidade) where T : class
+    {
+        using (var conexaoSql = CreateConnection())
+        {
+            conexaoSql.Insert<T>(entidade);
+            return entidade;
+        }
+    }
+
+    public List<T> CadastrarListagem<T>(List<T> entidades) where T : class
+    {
+        using (var conexaoSql = CreateConnection())
+        {
+            conexaoSql.Insert<List<T>>(entidades);
+            return entidades;
+        }
+    }
+
+    public T ObterPorId<T>(int entidadeId) where T : class
+    {
+        using(var conexaoSql = CreateConnection())
+        {
+            T entidade = conexaoSql.Get<T>(entidadeId);
+            return entidade;
+        }
+    }
+
+    public List<T> ObterTodos<T>() where T : class
+    {
+        using (var conexaoSql = CreateConnection())
+        {
+            List<T> listagem = conexaoSql.GetAll<T>().ToList();
+            return listagem;
+        }
+    }
+
+    public bool Atualizar<T>(T entidade) where T : class
+    {
+        using (var conexaoSql = CreateConnection())
+        {
+            bool operacaoAtualizacao = conexaoSql.Update<T>(entidade);
+            return operacaoAtualizacao;
+        }
+    }
+
+    public bool Excluir<T>(T entidade) where T : class
+    {
+        using (var conexaoSql = CreateConnection())
+        {
+            bool operacaoExclusao = conexaoSql.Delete<T>(entidade);
+            return operacaoExclusao;
+        }
+    }
+
+    public bool ExcluirPorId<T>(int entidadeId) where T : class
+    {
+        using (var conexaoSql = CreateConnection())
+        {
+            T entidade = conexaoSql.Get<T>(entidadeId);
+            bool operacaoExclusao = conexaoSql.Delete<T>(entidade);
+            
+            return operacaoExclusao;
+        }
+    }
+
+}
